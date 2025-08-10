7a77fd08360327a655a533461a5ba52ec46c6866
MaYa <armanalam2410@gmail.com>
2025-08-11 01:25:40 +0600

    undated link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ import { SocialIcon } from 'react-social-icons';
 const Footer = () => {
     const link = <>
             <ul className="flex justify-between text-lg font-medium">
-                <NavLink to="/" className={({isActive})=>isActive ? 'border-b-2 border-red-400 text-red-400 font-bold' : ""}>Home</NavLink>
-                        <li>My-Booking</li>
-                        <li>Blogs</li>
-                        <li>Contact Us</li>
+                        <NavLink to="/" className={({isActive})=>isActive ? 'border-b-2 border-red-400 text-red-400 font-bold' : ""}>Home</NavLink>
+                        <NavLink to="/booking" className={({isActive})=>isActive ? 'border-b-2 border-red-400 text-red-400 font-bold' : ""}><li>My-Booking</li></NavLink>
+                        <NavLink to="/blogs" className={({isActive})=>isActive ? 'border-b-2 border-red-400 text-red-400 font-bold' : ""}><li>Blogs</li></NavLink>
+                        <NavLink to="/contact" className={({isActive})=>isActive ? 'border-b-2 border-red-400 text-red-400 font-bold' : ""}><li>Contact Us</li></NavLink>
                     </ul>
                     </>
     return (
