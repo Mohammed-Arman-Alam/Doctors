2540faae5b42f632a22453752e41792be8a61ac1
MaYa <armanalam2410@gmail.com>
2025-08-11 00:24:34 +0600

    added graph

--- src/components/Pages/Booking/Booking.jsx
+++ src/components/Pages/Booking/Booking.jsx
@@ -1,7 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLoaderData } from 'react-router';
 import { getBookedAppoinment } from '../storage/addToDB';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+const getPath = (x, y, width, height) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+  Z`;
+};
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
 const Booking = () => {
     const data = useLoaderData();
     const [bookedDoctors, setBookedDoctors] = useState([]);
@@ -21,7 +78,22 @@ const Booking = () => {
     }
     return (
         <div className='w-10/12 mx-auto'>
-            <div>
+            <div className='bg-white rounded-xl mt-10 mb-16'>
+                <BarChart className='w-full'
+                width={1000}
+                height={400}
+                data={bookedDoctors}
+                margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+                >
+  <CartesianGrid strokeDasharray="3 3" />
+  <XAxis dataKey="name" />
+  <YAxis />
+  <Bar dataKey="consultancyFee" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+    {bookedDoctors.map((doctor, index) => (
+      <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+    ))}
+  </Bar>
+</BarChart>
             </div> 
             <div>
                 <h1 className='text-[40px] font-extrabold text-center my-3'>My Todays Appointments</h1>
